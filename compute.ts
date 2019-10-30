ddfd77cadeeb0afba4deef3fbb1e2debd88a12c9
Erin Krengel <ekrengel@pulumi.com>
2019-10-30 15:07:14 -0700

    fix merge

--- compute.ts
+++ compute.ts
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 import * as aws from "@pulumi/aws";
-<<<<<<< HEAD
 import { EnforcementLevel, Policy, typedRule } from "@pulumi/policy";
 
 import * as assert from "assert";
@@ -28,13 +27,6 @@ export const compute: Policy[] = [
 ];
 
 export function Ec2InstanceDetailedMonitoringEnabled(enforcementLevel: EnforcementLevel = "advisory"): Policy {
-=======
-import { typedRule, EnforcementLevel, Policy } from "@pulumi/policy";
-
-import * as assert from "assert";
-
-function Ec2InstanceDetailedMonitoringEnabled(enforcementLevel: EnforcementLevel = "advisory"): Policy {
->>>>>>> 3001d36a7ba1723d65e337ed142cb15ffe1f7fb2
     return {
         name: "ec2-instance-detailed-monitoring-enabled",
         description: "Checks whether detailed monitoring is enabled for EC2 instances.",
@@ -43,30 +35,10 @@ function Ec2InstanceDetailedMonitoringEnabled(enforcementLevel: EnforcementLevel
             typedRule(aws.ec2.Instance.isInstance, it => assert.ok(it.monitoring,
                 "You should enable detailed monitoring on EC2 instances.")),
         ],
-<<<<<<< HEAD
     };
 }
 
 export function Ec2InstanceNoPublicIP(enforcementLevel: EnforcementLevel = "advisory"): Policy {
-=======
-    }
-}
-
-// TODO
-// function Ec2InstanceManagedBySystemsManager(enforcementLevel: EnforcementLevel = "advisory"): Policy {
-//     return {
-//         name: "ec2-instance-managed-by-systems-manager",
-//         description: "Checks whether the Amazon EC2 instances in your account are managed by AWS Systems Manager.",
-//         enforcementLevel: enforcementLevel,
-//         rules: [
-//             typedRule(aws.ec2.Instance.isInstance, it => assert.ok(it.monitoring,
-//                 "You should enable detailed monitoring on EC2 instances.")),
-//         ],
-//     }
-// }
-
-function Ec2InstanceNoPublicIP(enforcementLevel: EnforcementLevel = "advisory"): Policy {
->>>>>>> 3001d36a7ba1723d65e337ed142cb15ffe1f7fb2
     return {
         name: "ec2-instance-no-public-ip",
         description: "Checks whether Amazon EC2 instances have a public IP association. " +
@@ -76,7 +48,6 @@ function Ec2InstanceNoPublicIP(enforcementLevel: EnforcementLevel = "advisory"):
             typedRule(aws.ec2.Instance.isInstance, it => assert.ok(!it.associatePublicIpAddress,
                 "An EC2 instance should not have a public IP associated with it.")),
         ],
-<<<<<<< HEAD
     };
 }
 
@@ -103,112 +74,6 @@ function Ec2VolumeInUseCheck(enforcementLevel: EnforcementLevel = "advisory", ch
 }
 
 export function ElbAccessLoggingEnabled(enforcementLevel: EnforcementLevel = "advisory"): Policy {
-=======
-    }
-}
-
-// TODO
-// function Ec2InstancesInVpc(enforcementLevel: EnforcementLevel = "advisory"): Policy {
-//     return {
-//         name: "ec2-instances-in-vpc",
-//         description: "Checks whether your EC2 instances belong to a virtual private cloud (VPC).",
-//         enforcementLevel: enforcementLevel,
-//         rules: [
-//             typedRule(aws.ec2.Instance.isInstance, it => assert.ok(!it.associatePublicIpAddress,
-//                 "An EC2 instance should not have a public IP associated with it.")),
-//         ],
-//     }
-// }
-
-// TODO
-// function Ec2ManagedInstanceApplicationsBlacklisted(enforcementLevel: EnforcementLevel = "advisory"): Policy {
-//     return {
-//         name: "ec2-managedinstance-applications-blacklisted",
-//         description: "Checks that none of the specified applications are installed on the instance. " +
-//             "Optionally, specify the application version. Newer versions of the application will not be blacklisted. " +
-//             "You can also specify the platform to apply the rule only to instances running that platform..",
-//         enforcementLevel: enforcementLevel,
-//         rules: [
-//             typedRule(aws.ssm., it => assert.ok(!it.associatePublicIpAddress,
-//                 "An EC2 instance should not have a public IP associated with it.")),
-//         ],
-//     }
-// }
-
-// TODO
-// function Ec2InstanceGroupAttachedToEni(enforcementLevel: EnforcementLevel = "advisory"): Policy {
-//     return {
-//         name: "ec2-security-group-attached-to-eni",
-//         description: "Checks that security groups are attached to Amazon EC2 instances or to an elastic network interface.",
-//         enforcementLevel: enforcementLevel,
-//         rules: [
-//             typedRule(aws.ec2.Instance.isInstance, it => assert.ok(((it.securityGroups && it.securityGroups.length > 0)
-//                 || (it.networkInterfaces && it.networkInterfaces.length > 0))),
-//         ],
-//     }
-// }
-
-// TODO
-// function Ec2VolumeInUseCheck(enforcementLevel: EnforcementLevel = "advisory", checkDeletion: boolean): Policy {
-//     return {
-//         name: "ec2-volume-inuse-check",
-//         description: "Checks whether EBS volumes are attached to EC2 instances. " +
-//             "Optionally checks if EBS volumes are marked for deletion when an instance is terminated.",
-//         enforcementLevel: enforcementLevel,
-//         rules: [
-//             typedRule(aws.ec2.Instance.isInstance, it => assert.ok(
-//                 it.ebsBlockDevices && it.ebsBlockDevices.length > 0,
-//                 "EC2 instance has no EBS volumes attached.")),
-//             typedRule(aws.ec2.Instance.isInstance, it => assert.ok(it => {
-//                 if (!checkDeletion) {
-//                     return true;
-//                 }
-//                 for (const vol of it.ebsBlockDevices) {
-//                     if (vol.deleteOnTermination) {
-
-//                     }
-//                 }
-//             })),
-//         ],
-//     }
-// }
-
-// Cant do
-// function EipAttached(enforcementLevel: EnforcementLevel = "advisory"): Policy {
-//     return {
-//         name: "eip-attached",
-//         description: "Checks whether all Elastic IP addresses that are allocated to a VPC are attached to EC2 instances or in-use elastic network interfaces (ENIs). Results might take up to 6 hours to become available after an evaluation occurs.",
-//         enforcementLevel: enforcementLevel,
-//         rules: [
-//             typedRule(aws.ec2.Instance.isInstance, it => assert.ok(!it.associatePublicIpAddress,
-//                 "An EC2 instance should not have a public IP associated with it.")),
-//         ],
-//     }
-// }
-
-// TODO: This isn't quite right. We need to get the ID and make sure its from ACM. 
-// Also if it is set this is only valid when the lbProtocol is HTTPS or SSL, so we will
-// want to check that as well.
-function ElbAcmCertificateRequired(enforcementLevel: EnforcementLevel = "advisory"): Policy {
-    return {
-        name: "elb-acm-certificate-required",
-        description: "Checks whether the Classic Load Balancers use SSL certificates provided by AWS Certificate Manager. " +
-            "To use this rule, use an SSL or HTTPS listener with your Classic Load Balancer. " +
-            "This rule is only applicable to Classic Load Balancers." +
-            "This rule does not check Application Load Balancers and Network Load Balancers.",
-        enforcementLevel: enforcementLevel,
-        rules: [
-            typedRule(aws.elasticloadbalancing.LoadBalancer.isInstance, it => {
-                for (const listener of it.listeners) {
-                    assert.ok(listener.sslCertificateId);
-                }
-            }),
-        ],
-    }
-}
-
-function ElbAccessLoggingEnabled(enforcementLevel: EnforcementLevel = "advisory"): Policy {
->>>>>>> 3001d36a7ba1723d65e337ed142cb15ffe1f7fb2
     return {
         name: "elb-logging-enabled",
         description: "Checks whether the Application Load Balancers and the Classic Load Balancers have logging enabled.",
@@ -221,38 +86,10 @@ function ElbAccessLoggingEnabled(enforcementLevel: EnforcementLevel = "advisory"
             typedRule(aws.applicationloadbalancing.LoadBalancer.isInstance,
                 it => assert.ok(it.accessLogs && it.accessLogs.enabled, "An ALB should have access logs enabled.")),
         ],
-<<<<<<< HEAD
     };
 }
 
 export function EncryptedVolumes(enforcementLevel: EnforcementLevel = "advisory", kmsId?: string): Policy {
-=======
-    }
-}
-
-// CANT DO -- this requires we are able to get a `LoadBalancerPolicy` and `LoadBalancer`
-// function ElbPredefinedSecurityPolicySSLCheck(enforcementLevel: EnforcementLevel = "advisory", predefinedPolicyName: string): Policy {
-//     return {
-//         name: "elb-predefined-security-policy-ssl-check",
-//         description: "Checks whether your Classic Load Balancer SSL listeners are using a predefined policy. " +
-//             "The rule is only applicable if there are SSL listeners for the Classic Load Balancer.",
-//         enforcementLevel: enforcementLevel,
-//         rules: [
-//             typedRule(aws.elasticloadbalancing.LoadBalancer.isInstance, it => {
-                
-//                 for (const listener of it.listeners) {
-//                     if (listener.lbProtocol && listener.lbProtocol == "SSL") {
-//                     }
-//                 }
-//             }),
-//             typedRule(aws.elasticloadbalancingv2.LoadBalancer.isInstance,
-//                 it => assert.ok(it.accessLogs && it.accessLogs.enabled, "An ELB should have access logs enabled.")),
-//         ],
-//     }
-// }
-
-function EncryptedVolumes(enforcementLevel: EnforcementLevel = "advisory", kmsId?: string): Policy {
->>>>>>> 3001d36a7ba1723d65e337ed142cb15ffe1f7fb2
     return {
         name: "encrypted-volumes",
         description: "Checks whether the EBS volumes that are in an attached state are encrypted. " +
@@ -264,24 +101,13 @@ function EncryptedVolumes(enforcementLevel: EnforcementLevel = "advisory", kmsId
                 it => {
                     if (it.ebsBlockDevices && it.ebsBlockDevices.length > 0) {
                         for (const ebs of it.ebsBlockDevices) {
-<<<<<<< HEAD
                             assert.ok(ebs.encrypted, "EBS volumes should be encrypted.");
                             if (kmsId) {
                                 assert.ok(ebs.kmsKeyId === kmsId, "EBS volume not encrypted with required key.");
-=======
-                            assert.ok(ebs.encrypted, "EBS volumes should be encrypted.")
-                            if (kmsId) {
-                                assert.ok(ebs.kmsKeyId === kmsId, "EBS volume not encrypted with required key.")
->>>>>>> 3001d36a7ba1723d65e337ed142cb15ffe1f7fb2
                             }
                         }
                     }
                 }),
         ],
-<<<<<<< HEAD
     };
 }
-=======
-    }
-}
->>>>>>> 3001d36a7ba1723d65e337ed142cb15ffe1f7fb2
