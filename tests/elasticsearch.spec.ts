ce959ad647b26da5d176ea38b911140d01c95bb1
Erin Krengel <ekrengel@pulumi.com>
2019-11-08 15:35:20 -0800

    Get elastic search unit tests working

--- tests/elasticsearch.spec.ts
+++ tests/elasticsearch.spec.ts
@@ -17,14 +17,69 @@ import "mocha";
 import * as aws from "@pulumi/aws";
 
 import * as elasticsearch from "../elasticsearch";
-import { assertHasViolation, assertNoViolations, fakeResource } from "./util";
+import { assertHasViolation, assertNoViolations, fakeResource } from './util';
+
+describe("#ElasticsearchEncryptedAtRest", () => {
+    const policy = elasticsearch.ElasticsearchEncryptedAtRest("mandatory");
+    const policyArgs = {
+        type: (<any>aws.elasticsearch.Domain).__pulumiType,
+        props: {},
+    };
+    it("Should fail if domain's encryptAtRest is undefined", async () => {
+        const domainUnderTest = fakeResource<aws.elasticsearch.Domain>({
+            domainName: "test-name",
+        });
+        policyArgs.props = domainUnderTest;
+
+        const msg = `Elasticsearch domain ${domainUnderTest.domainName} must be encrypted at rest.`;
+        await assertHasViolation(policy, policyArgs, { message: msg });
+    });
+    it("Should fail if domain's encryptAtRest is disabled", async () => {
+        const domainUnderTest = fakeResource<aws.elasticsearch.Domain>({
+            domainName: "test-name",
+            encryptAtRest: {
+                enabled: false,
+            },
+        });
+        policyArgs.props = domainUnderTest;
+
+        const msg = `Elasticsearch domain ${domainUnderTest.domainName} must be encrypted at rest.`;
+        await assertHasViolation(policy, policyArgs, { message: msg });
+    });
+
+    it("Should pass if encryptAtRest is enabled", async () => {
+        const domainUnderTest = fakeResource<aws.elasticsearch.Domain>({
+            domainName: "test-name",
+            encryptAtRest: {
+                enabled: true,
+            },
+        });
+        policyArgs.props = domainUnderTest;
+
+        await assertNoViolations(policy, policyArgs);
+    });
+});
 
 describe("#ElasticsearchInVpcOnly", () => {
+    const policy = elasticsearch.ElasticsearchInVpcOnly("mandatory");
+    const domainType = (<any>aws.elasticsearch.Domain).__pulumiType;
     it("Should fail if no VPC options are available", async () => {
-        const policy = elasticsearch.ElasticsearchInVpcOnly("mandatory");
         const noVpc = fakeResource<aws.elasticsearch.Domain>({});
-        assertHasViolation(policy, noVpc, { message: "must run within a VPC." });
+        const policyArgs = {
+            type: domainType,
+            props: noVpc,
+        };
+
+        await assertHasViolation(policy, policyArgs, { message: "must run within a VPC." });
+    });
+
+    it("Should pass if VPC options are available", async () => {
         const vpc = fakeResource<aws.elasticsearch.Domain>({ vpcOptions: {} });
-        assertNoViolations(policy, vpc);
+        const vpcPolicyArgs = {
+            type: domainType,
+            props: vpc,
+        };
+
+        await assertNoViolations(policy, vpcPolicyArgs);
     });
 });
