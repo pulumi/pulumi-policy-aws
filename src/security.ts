37910a1aeab368e828ff73187309e9a4774c9609
Tushar Shah <tushar.shah1@gmail.com>
2021-06-15 13:33:41 -0400

    hardcoded region

--- src/security.ts
+++ src/security.ts
@@ -29,8 +29,8 @@ import { defaultEnforcementLevel } from "./enforcementLevel";
 import { PolicyArgs } from "./policyArgs";
 import { Config } from "@pulumi/pulumi";
 
-const myregion = aws.config.region;
-
+//const myregion = aws.config.region;
+const myregion = "us-east-2";
 // Mixin additional properties onto AwsGuardArgs.
 declare module "./awsGuard" {
     interface AwsGuardArgs {
